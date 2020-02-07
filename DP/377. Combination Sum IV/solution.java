7378b07ff523a0f33b7fce6133abe83034e0517d
KetaoChen <kchen343@gatech.edu>
2020-02-06 20:19:22 -0500

    dfs memo | dp

--- DP/377. Combination Sum IV/solution.java
+++ DP/377. Combination Sum IV/solution.java
@@ -5,6 +5,41 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         //dp[i] is the number of ways to get i.        
+        int[] memo = new int[target + 1];
+		//must do this
+        Arrays.fill(memo, -1);
+        //initialize
+        memo[0] = 1;
+        Arrays.sort(nums);
+        return helper(nums, target, memo);
+    }
+    private int helper(int[] nums, int target, int[] memo) {
+		if (target < 0 && target >= memo.length) {
+			return 0;
+		}
+        if (memo[target] != -1) {
+            return memo[target];
+        }
+        int res = 0;
+        for (int num : nums) {
+			//must do this, or we need to check the index in the beginning of the method.
+            if (num > target) {
+                break;
+            }
+            res += helper(nums, target - num, memo);
+        }
+        memo[target] = res;
+        return res;
+    }
+}
+
+// OJ: https://leetcode.com/problems/combination-sum-iv/
+// Author: https://leetcode.com/charlesna/
+// Time: O(target*n)
+// Space: O(target)
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        //dp[i] is the number of ways to get i.        
         int[] dp = new int[target + 1];
         //initialize
         dp[0] = 1;
