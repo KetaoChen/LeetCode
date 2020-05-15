992a4b0532218352f401ef4dbbfeea4b9ed37fa5
KetaoChen <kchen343@gatech.edu>
2020-05-15 09:20:53 -0400

    Revert "stack | parenthesis counter"
    This reverts commit 33a8721d1b9a6d4344c315972398329a441e27f6.

--- Stack/32. Longest Valid Parentheses/solution.java
+++ Stack/32. Longest Valid Parentheses/solution.java
@@ -1,50 +1,6 @@
 // OJ: https://leetcode.com/problems/longest-valid-parentheses/
 // Author: https://leetcode.com/charlesna/
 // Time: O(n)
-// Space: O(1)
-class Solution {
-    public int longestValidParentheses(String s) {
-        int left = 0, right = 0, res = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                left++;
-            }
-            else {
-                right++;
-            }
-            if (left == right) {
-                res = Math.max(res, left * 2);
-            }
-            if (right > left) {
-                left = 0;
-                right = 0;
-            }
-        }
-        left = 0;
-        right = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                left++;
-            }
-            else {
-                right++;
-            }
-            if (left == right) {
-                res = Math.max(res, left * 2);
-            }
-            if (left > right) {
-                left = 0;
-                right = 0;
-            }
-        }
-        return res;
-    }
-}
-
-// OJ: https://leetcode.com/problems/longest-valid-parentheses/
-// Author: https://leetcode.com/charlesna/
-// Time: O(n)
 // Space: O(n)
 class Solution {
     public int longestValidParentheses(String s) {
@@ -70,4 +26,3 @@ class Solution {
         return res;
     }
 }
-
